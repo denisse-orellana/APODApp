913daa7b9511e58ff67541b996024df175bd2f53
Denisse Orellana <denissep.orellana@gmail.com>
2023-02-21 12:03:39 -0300

    change: vite config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const START_DATE = "2023-01-01";
 
 const BASE_URL_APOD = "https://api.nasa.gov/planetary/apod";
-const BASE_API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_API_KEY = process.env.VITE_API_KEY;
 
 const BASE_URL_SEARCH = "https://images-api.nasa.gov/";
 const MEDIA_TYPE = "video";
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+export default (({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: parseInt(process.env.VITE_PORT),
+    },
+    preview: {
+      port: 4270,
+    },
+    define: {
+      'process.env': process.env,
+    },
+    build: {
+      incremental: true,
+      babel: {
+        presets: [ "@babel/present-env", "@babel/preset-react"],
+      },
+      cache: true,
+      minify: true,
+      mode: "production",
+      chunks: true,
+      moduleBundling: true,
+      debug: true,
+    }
+  })
 })
